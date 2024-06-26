dd6686e34934cd9d6a57b8a9cae775f71e87fb41
colorlessdia <colorlessdia@gmail.com>
2024-06-26 15:03:29 +0900

    [feat] global css 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { Global, css } from "@emotion/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Global styles={css({
+      '*, *::before, *::after': {
+        margin: 0,
+        padding: 0,
+        boxSizing: "border-box",
+      },
+      'ul li': {
+        listStyle: "none",
+      },
+      'a': {
+        color: 'inherit',
+        textDecoration: 'none',
+      }
+    })} />
     <App />
   </React.StrictMode>,
 );
