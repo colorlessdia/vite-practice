037c714f0ecf23ecfdd96f86171e8d86f098d1ad
colorlessdia <colorlessdia@gmail.com>
2024-06-26 15:21:06 +0900

    [refactor] global CSS 파일 분리

---
+++ src/globalStyle.ts
@@ -0,0 +1,18 @@
+import { css } from "@emotion/react";
+
+const globalStyle = css({
+  '*, *::before, *::after': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+  'ul li': {
+    listStyle: 'none',
+  },
+  'a': {
+    color: 'inherit',
+    textDecoration: 'none',
+  }
+});
+
+export default globalStyle;
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { Global, css } from "@emotion/react";
+// styles
+import { Global } from "@emotion/react";
+import globalStyle from "./globalStyle.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Global styles={css({
-      '*, *::before, *::after': {
-        margin: 0,
-        padding: 0,
-        boxSizing: "border-box",
-      },
-      'ul li': {
-        listStyle: "none",
-      },
-      'a': {
-        color: 'inherit',
-        textDecoration: 'none',
-      }
-    })} />
+    <Global styles={globalStyle} />
     <App />
   </React.StrictMode>,
 );
